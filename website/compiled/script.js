d7743fa110c7020a6adc6db365bdb2841c61416a
Emma <contact@emma.pizza>
2025-04-24 13:22:19 +0200

    website: bump tsc target es5 -> es6

--- website/compiled/script.js
+++ website/compiled/script.js
@@ -1,5 +1,5 @@
-var logoScroll = 60;
-var logoElement;
+let logoScroll = 60;
+let logoElement;
 function init() {
     var _a;
     logoElement = (_a = document.getElementById("logo")) !== null && _a !== void 0 ? _a : document.body; // Stop annoying me VSCode. This is to shut you up
