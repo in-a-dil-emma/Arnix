bd6c50e26965b971aebc72ef6a4620168b7c9f3c
Emma <contact@emma.pizza>
2025-07-13 13:29:54 +0200

    update website stuff

--- website/script.ts
+++ website/script.ts
@@ -2,10 +2,10 @@ let logoScroll: number = 60
 let logoElement: HTMLElement;
 
 function init() {
-    logoElement = document.getElementById("logo") ?? document.body // Stop annoying me VSCode. This is to shut you up
-    window.addEventListener("scroll", handlescroll)
+    logoElement = document.getElementById("logo")!;
+    window.addEventListener("scroll", handlescroll);
 }
 
 function handlescroll() {
-    logoElement.style.opacity = ((logoScroll - window.scrollY) / logoScroll).toString()
+    logoElement.style.opacity = ((logoScroll - window.scrollY) / logoScroll).toString();
 }
